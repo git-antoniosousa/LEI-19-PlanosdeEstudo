9ce88afb383d49fa453c1731655dd8a79db97b3a
Cláudia Correia <a77431@alunos.uminho.pt>
2019-04-30 19:53:56 +0100

    Inscrição do aluno falhada

--- PlanUM/custom-addons/planum/models/aluno.py
+++ PlanUM/custom-addons/planum/models/aluno.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Aluno(models.Model):
     _inherit='res.partner'
@@ -11,8 +11,24 @@ class Aluno(models.Model):
     nr_mecanografico=fields.Char('Nº Mecanográfico')
     media_acesso=fields.Float('Média Acesso',(5,3))
     estatuto=fields.Char('Estatuto')
-    #Tirar?
-    #regime=fields.Char('Regime')
     ano = fields.Integer('Ano')
     plano_estudos_id = fields.Many2one('planum.plano_estudos', 'Plano Estudos ID')
     curso_id = fields.Many2one('planum.curso', 'Curso ID')
+
+    @api.model
+    def create(self, vals):
+        curso_id = vals['curso_id']
+        curso = self.env['planum.curso'].browse(curso_id)
+
+        # Define plano de estudos
+        vals['plano_estudos_id'] = curso.plano_atual()
+
+        new_record = super().create(vals)
+
+        # Add security group to aluno
+        security_group = self.env.ref('planum.planum_group_aluno')
+        security_group.write({
+            'users': [(4, res.user_id.id)]
+        })
+
+        return new_record
--- PlanUM/custom-addons/planum/models/curso.py
+++ PlanUM/custom-addons/planum/models/curso.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Curso(models.Model):
     _name = 'planum.curso'
@@ -12,3 +12,15 @@ class Curso(models.Model):
     alunos=fields.One2many('planum.aluno', 'curso_id', 'Alunos')
     planos_curso = fields.One2many('planum.plano_curso', 'curso_id', 'Planos de Curso')
     direcao_curso = fields.One2many('planum.direcao_curso', 'curso_id', 'Direção de Curso')
+
+    @api.one
+    def plano_atual(self):
+        planos_curso = self.env['planum.plano_curso'].search([])
+        max_date = None
+
+        for plano in planos_curso:
+            if not max_date or plano.data_fim >= max_date:
+                max_date = plano.data_fim
+                plano_atual = plano
+
+        return plano_atual
